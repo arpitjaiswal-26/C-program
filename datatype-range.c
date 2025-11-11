02693fa78de565d11326aada56079c62c5089006
arpitjaiswal-26 <arpitjaiswal546@gmail.com>
2025-11-11 13:18:39 +0530

    Add files via upload

---
+++ datatype-range.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+
+int main(){
+    printf("diffrent data types and its range\n");
+    printf("int:range %d to %d\n",INT_MIN,INT_MAX);
+     printf("char:range %c to %c\n",CHAR_MIN,CHAR_MAX);
+    printf("int:range  %f to %f\n ",FLT_MIN,FLT_MAX);
+    return 0;
+}
